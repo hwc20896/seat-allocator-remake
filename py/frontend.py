e4976188577befa8eca5a3df4c425bd11efed3fc
hwc20896 <hwc20896@gmail.com>
2025-10-26 18:47:25 +0800

    init python side backend

--- py/frontend.py
+++ py/frontend.py
@@ -1,14 +1,20 @@
-from PyQt6.QtWidgets import QWidget, QFileDialog, QMessageBox, QTableWidgetItem
+from PyQt6.QtWidgets import QWidget, QFileDialog, QMessageBox, QTableWidgetItem, QHeaderView
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFont
 
 from ui.ui_widget import Ui_Widget
 import pandas as pd
+import os
+
+import grid_shuffler
 
 class Frontend(QWidget, Ui_Widget):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        # Backend class
+        self.__backend = grid_shuffler.Backend()
 
         # Window initialization
         self.setFixedSize(1000,750)
@@ -18,6 +24,13 @@ class Frontend(QWidget, Ui_Widget):
         self.tab_display.removeTab(1)
         self.tab_display.setTabText(0, '原始')
         del self.tab_2
+
+        # Grid displayer config
+        self.seat_viewer.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        self.seat_viewer.verticalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+
+        # Button connectors
+        self.csv_import.clicked.connect(self.import_data)
 
     #   __init__(self)  ^^^^^
     #  =============================================================
@@ -38,14 +51,23 @@ class Frontend(QWidget, Ui_Widget):
         print(f'Selected file: {file_name}')
         try:
             data = read_csv(file_name)
+            self.display_data(data)
+
+            self.csv_import_state.setText("已導入")
+            self.csv_import.setText(os.path.basename(file_name))
+            self.begin_shuffle.setEnabled(True)
         except RuntimeError:
             QMessageBox.warning(self, "錯誤", "讀取 CSV 檔案失敗。")
 
     #   import_data(self):
     #  =============================================================
 
-    def display_data(self, d: int | list[list[int]]):
-        data = d if not isinstance(d, int) else []
+    def display_data(self, d: int | list[list[str]]):
+        try:
+            data = self.__backend.set_data(d) if not isinstance(d, int) else self.__backend.get_grid(d)
+        except Exception as e:
+            print(f"backend failed with error: {e}")
+            return
 
         if not data: return
 
